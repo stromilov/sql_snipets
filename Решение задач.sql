cad3eba3c9599c32f66fd0944e8a22e39ad6a7d9
Nick <stromilovnikolay@gmail.com>
2023-06-23 12:25:01 +0000

    Update Решение задач.sql

--- Решение задач.sql
+++ Решение задач.sql
@@ -1,9 +1,19 @@
 1.1
 --MySQL
 create table Database(
-  id      int primary key auto_increment,
+  id      int          primary key auto_increment,
   name    varchar(30),
   version varchar(30)
 );
 
 1.2
+
+1.4
+--MySQL
+create table TV(
+  id       int           primary key auto_increment,
+  model    varchar(30),
+  maker    varchar(30),
+  diagonal decimal(3,2),
+  price    decimal(12,2)
+);
