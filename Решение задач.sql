edbf177ff649632a4ba0d254ddce73521101ef6d
Nick <stromilovnikolay@gmail.com>
2023-07-07 19:25:33 +0000

    Update Решение задач.sql

--- Решение задач.sql
+++ Решение задач.sql
@@ -48,3 +48,78 @@ insert into TV(model, maker, diagonal, price) values
 
 1.6
 --MySQL
+
+
+
+
+Создать таблицы интернет магазина книг:
+--MySQL
+create table author (
+    author_id   int primary key auto_increment,
+    name_author varchar(30)
+    );
+
+create table genre (
+    genre_id   int primary key auto_increment,
+    name_genre varchar(30)
+    );
+
+create table book(
+    book_id   int          primary key auto_increment,
+    title     varchar(30),
+    author_id int          not null,
+    genre_id  int          not null,
+    price     decimal(8,2),
+    amount    int, 
+    foreign key (author_id) references author (author_id) on delete cascade,
+    foreign key (genre_id)  references genre  (genre_id)  on delete cascade
+    );
+
+create table city(
+    city_id       int primary key auto_increment,
+    name_city     varchar(30),
+    days_delivery int
+    );
+
+create table client(
+    client_id   int primary key auto_increment,
+    name_client varchar(50),
+    city_id     int,
+    email       varchar(30),
+    foreign key (city_id) references city (city_id) on delete cascade
+    );
+
+create table buy2(
+    buy_id          int primary key auto_increment,
+    buy_description varchar(100),
+    client_id       int,
+    foreign key (client_id) references client (client_id) on delete cascade
+    );
+    
+create table buy_book(
+    buy_book_id int primary key auto_increment,
+    buy_id      int,
+    book_id     int,
+    amount      int,
+    foreign key (buy_id) references buy (buy_id) on delete cascade,
+    foreign key (book_id) references book (book_id) on delete cascade
+    );
+    
+create table step(
+    step_id   int primary key auto_increment,
+    name_step varchar(30)
+    );
+    
+create table buy_step(
+    buy_step_id   int primary key auto_increment,
+    buy_id        int,
+    step_id       int,
+    date_step_beg date,
+    date_step_end date,
+    foreign key (buy_id) references buy (buy_id) on delete cascade,
+    foreign key (step_id) references step (step_id) on delete cascade
+    );
+
+Заполнить таблицы интернет магазина книг данными:
+--MySQL
+insert 
